14fd1aefa1623cc0f888c54cfe5f644fa0daf716
Divium <79859179+Divium-l@users.noreply.github.com>
2023-04-26 20:02:51 +0300

    idk

---
+++ src/components/Login.jsx
@@ -0,0 +1,53 @@
+import { Show, createSignal } from "solid-js"
+
+export default function Login() {
+    const [email, setEmail] = createSignal("")
+    const [password, setPassword] = createSignal("")
+    const [status, setStatus] = createSignal("")
+
+    const login = () => {
+        console.log("Login")
+    }
+
+    return (
+        <div className="hero min-h-full bg-base-100">
+            <div className="hero-content flex-row lg:flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Траектория</h1>
+                    <p className="py-6">
+                        Масштабный проект «Траектория» от AdIndex создан для студентов, джуниоров и тех, кто 
+                        стремится достичь успеха в индустрии маркетинга и коммуникаций. Конференция «Траектория.
+                    </p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-200">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Почта</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered"/>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Пароль</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Забыли пароль?</a>
+                            </label>
+                        </div>
+                        <Show when={status() === "error"}>
+                            <p className="bg-error">Неверный пароль</p>
+                        </Show>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary" onClick={login}>Войти</button>
+                        </div>
+                        <div>
+                            <a href="registration" className="label-text-alt link link-hover">Регистрация</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,15 @@ module.exports = {
     themes: [
       {
         mainTheme: {
-          primary: '#f2f2f2',
+          primary: '#2f7fb0',
           secondary: '#b8e419',
+          "base-100": "#ffffff",
+          "base-200": "#f2f2f2",
+          error: "#d0363d",
+          accent: "#2f7fb0",
+          neutral: "#1b1b1b",
         },
       },
     ],
-    trajectory: {
-      primary: "#a991f7",
-      secondary: "#f6d860",
-      accent: "#37cdbe",
-      neutral: "#3d4451",
-    }
   }
 }
