215f9f78b412c993f23f1105de47b53bd231a025
Egor-Always-AFK <shokolokill@gmail.com>
2023-04-26 14:28:23 +0300

    Add banner to lending page and add some code in cofig

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
+    themes: [
+      {
+        mainTheme: {
+          primary: '#f2f2f2',
+          secondary: '#b8e419',
+        },
+      },
+    ],
     trajectory: {
       primary: "#a991f7",
       secondary: "#f6d860",
