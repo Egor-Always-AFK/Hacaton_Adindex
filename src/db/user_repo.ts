1360d3d89cbf09260ab263c5131364f82ae4544b
Divium <79859179+Divium-l@users.noreply.github.com>
2023-04-26 15:14:05 +0300

    profile

--- src/db/user_repo.ts
+++ src/db/user_repo.ts
@@ -3,7 +3,9 @@ import type { User } from "../model/user";
 let userList: Array<User> = [];
 
 export function saveProfile(profile: User): void {
+    profile.id = Math.floor(Math.random() * Number.MAX_VALUE)
     userList.push(profile);
+    console.log(profile)
 }
 
 export function findProfile(email: string, password: string) {
